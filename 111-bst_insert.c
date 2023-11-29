da24fc046d050203c860a1f8cddbf82b66860f49
LameckL <88710250+LameckL@users.noreply.github.com>
2023-11-29 19:09:14 +0000

    111

---
+++ 111-bst_insert.c
@@ -0,0 +1,47 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - inserts a value in a BST
+ * @tree: double ptr to the root node of the BST to insert the value
+ * @value: value to store in the node to be inserted
+ * Return: ptr to the created node, NULL - failure
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *curr, *new;
+
+	if (tree != NULL)
+	{
+		curr = *tree;
+
+		if (curr == NULL)
+		{
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
+		}
+
+		if (value < curr->n) /* insert in left subtree */
+		{
+			if (curr->left != NULL)
+				return (bst_insert(&curr->left, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->left = new);
+		}
+		if (value > curr->n) /* insert in right subtree */
+		{
+			if (curr->right != NULL)
+				return (bst_insert(&curr->right, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->right = new);
+		}
+	}
+	return (NULL);
+}
